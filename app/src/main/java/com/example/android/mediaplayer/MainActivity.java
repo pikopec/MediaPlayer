92873173f4b1a30e4098845ec53fb95c506b8d18
Piotr Kopec <kopecpiotr93@gmail.com>
2016-08-27 23:21:36 +0200

    Add a media player to play music

--- app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.mediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** Add a MediaPlayer instance to manage an audio file in
+         * the app.
+         */
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
+    }
+
+    public void playSong(View v ){
+        mediaPlayer.start();
+    }
+
+    public void pauseSong(View v ){
+        mediaPlayer.pause();
+    }
+
+    public void resetSong(View v ){
+        mediaPlayer.stop();
     }
 }
